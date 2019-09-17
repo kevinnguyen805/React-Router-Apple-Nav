9bad2ea5adf7e6f08f6cab8edd2254cc0035786c
admin <admin@admins-MacBook-Pro.local>
2019-09-17 15:30:44 -0700

    react router dom added and wrapped around app

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import apple from './apple.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <img src={apple} alt="apple logo" />
+        
+      </nav>
+
+
     </div>
   );
 }
 
 export default App;
+
+/*
+You need to build out a few components here
+
+1. variation of a NavWrapper component  (navigation header)
+
+2. Reusable Nav component (turn into a navigation link for each parent category)
+
+3. SubNav component (reusable and display sub-navigation link)
+
+
+Planning phase
+1. build the main top header containing Mac/iPad/Phone/Watch
+    * each is a nav-link that 
+
+*/
